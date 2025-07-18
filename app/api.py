2f03ffc4a401d6963696d4a7a5cb380a43572556
Gia Nghi Dang <olaadang@Mac.lan>
2025-07-18 17:16:43 -0500

    Third try with Lasso Regression on new dataset

--- app/api.py
+++ app/api.py
@@ -2,36 +2,25 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import pandas as pd 
-from app.preprocessing import preprocess_input  
 
-model = joblib.load('app/model/ridge_model.joblib')
-original_df = pd.read_csv('data/weather_training_data.csv')
+model = joblib.load('app/model/lasso_model.joblib')
+df = pd.read_csv('data/meteostat.csv')
+features = model.feature_names_in_
 
 app = FastAPI()
 
 class WeatherInput(BaseModel):
     date: str
+    tavg: float
     tmax: float
     tmin: float
-    prcp: float = 0
-    snow: float = 0
-    snwd: float = 0
+    pres: float
 
 @app.post("/predict")
 def predict_weather(data: WeatherInput):
     try:
-        # convert input data to DataFrame
-        new_row = pd.DataFrame([data.model_dump()])  # model_dump() converts Pydantic model to dict
-        # combine with original data
-        df_combined = pd.concat([original_df, new_row], ignore_index=True)
-        # preprocess the combined data
-        preprocessed_df = preprocess_input(df_combined)
-        # pick the input row for prediction
-        input_row = preprocessed_df.iloc[-1:]
-        # define predictor variables
-        X = model.feature_names_in_
-        # make prediction
-        prediction = model.predict(input_row[X])[0]
-        return {"predicted tmax": int(prediction)}
+        input_row = pd.DataFrame([data.model_dump()])[list(features)]
+        prediction = model.predict(input_row)[0]
+        return {"predicted tmax": round(prediction, 1)}
     except Exception as e:
         return {"error": str(e)}
